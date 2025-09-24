10972a18e02ef0f2518ce476cd9a7ca56ba7fe70
hencci <hencci20@gmail.com>
2025-09-24 08:52:48 +0100

    Destructuring props

---
+++ props/propsDestructuring.jsx
@@ -0,0 +1,20 @@
+// Unpacking your props in the component arguments allows for more concise and readable code.
+
+function Button({ text, color, fontSize }) {
+  const buttonStyle = {
+    color: color,
+    fontSize: fontSize + "px",
+  };
+
+  return <button style={buttonStyle}>{text}</button>;
+}
+
+export default function App() {
+  return (
+    <div>
+      <Button text="Click Me!" color="blue" fontSize={12} />
+      <Button text="Don't Click Me!" color="red" fontSize={12} />
+      <Button text="Click Me!" color="blue" fontSize={20} />
+    </div>
+  );
+}
