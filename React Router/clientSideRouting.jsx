a98e41029552f28908d4427b4193e6cc3db644e2
hencci <hencci20@gmail.com>
2025-10-15 07:04:00 +0100

    About client-side routing

---
+++ React Router/clientSideRouting.jsx
@@ -0,0 +1,5 @@
+// Client-side routing is the type of routing where JavaScript takes over the duty of handling the routes in an application.
+
+// Client-side routing helps in building single-page applications (SPAs) without refreshing as the user navigates.
+
+// For example, when a user clicks a navbar element, the URL changes and the view of the page is modified accordingly, within the client.
