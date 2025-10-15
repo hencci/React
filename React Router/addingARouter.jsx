b79b3fcb4edce887a0635aa995f99c0fc3e293ae
hencci <hencci20@gmail.com>
2025-10-15 09:18:50 +0100

    Using React Router custom Link element

--- React Router/addingARouter.jsx
+++ React Router/addingARouter.jsx
@@ -28,3 +28,31 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
   </StrictMode>
 );
+
+{
+  /*   The link element   */
+}
+
+//  React Router exports a custom Link element to be used instead of the regular a tag.
+
+import { Link } from "react-router"; //Profile.jsx file
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default App;
+
+// And now, we don’t get the browser reloading every time we click the link on the navbar!
