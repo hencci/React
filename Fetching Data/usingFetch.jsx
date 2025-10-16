2657d709131858f84f9d9a7ba8194f2c13871f7b
hencci <hencci20@gmail.com>
2025-10-16 16:05:38 +0100

    Handling errors

--- Fetching Data/usingFetch.jsx
+++ Fetching Data/usingFetch.jsx
@@ -4,12 +4,18 @@ import { useEffect, useState } from "react";
 
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/photos")
-      .then((response) => response.json())
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => {
+        if (response.status >= 400) {
+          throw new Error("server error");
+        }
+        return response.json();
+      })
       .then((response) => setImageURL(response[0].url))
-      .catch((error) => console.error(error));
+      .catch((error) => setError(error));
   }, []);
 
   return (
