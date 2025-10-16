b582694ad6368cdb0eb742f0974f4d732f9f97a1
hencci <hencci20@gmail.com>
2025-10-16 07:33:15 +0100

    Add an index property with the DefaultProfile as a child to the /profile route

--- React Router/nestedRoutes.jsx
+++ React Router/nestedRoutes.jsx
@@ -43,6 +43,7 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
 import App from "./App";
 import Profile from "./Profile";
+import DefaultProfile from "./DefaultProfile";
 import Spinach from "./Spinach";
 import Popeye from "./Popeye";
 
@@ -55,6 +56,7 @@ const router = createBrowserRouter([
     path: "profile",
     element: <Profile />,
     children: [
+      { index: true, element: <DefaultProfile /> },
       { path: "spinach", element: <Spinach /> },
       { path: "popeye", element: <Popeye /> },
     ],
@@ -64,5 +66,6 @@ const router = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
+  ss
 );
