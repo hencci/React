8a26ce05be6fb4eafbb32511fc0ec9109bfff83c
hencci <hencci20@gmail.com>
2025-10-16 07:28:15 +0100

    Create DefaultProfile component

---
+++ React Router/DefaultProfile.jsx
@@ -0,0 +1,7 @@
+// If you want to render something as a default component when no path is added to Profile, you can add an index route to the children!
+
+const DefaultProfile = () => {
+  return <p>Oh, nothing to see here!</p>;
+};
+
+export default DefaultProfile;
