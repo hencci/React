e3e7aee0d637ec08e61a2059284cc4fed8ae2400
hencci <hencci20@gmail.com>
2025-10-16 16:18:35 +0100

    Add loading stateto check request status

--- Fetching Data/usingFetch.jsx
+++ Fetching Data/usingFetch.jsx
@@ -1,10 +1,9 @@
 // Whenever a component needs to make a request as it renders, it’s often best to wrap that fetch inside of an effect.
 
-import { useEffect, useState } from "react";
-
 const Image = () => {
   const [imageURL, setImageURL] = useState(null);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
@@ -15,16 +14,18 @@ const Image = () => {
         return response.json();
       })
       .then((response) => setImageURL(response[0].url))
-      .catch((error) => setError(error));
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
   }, []);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
+
   return (
-    imageURL && (
-      <>
-        <h1>An image</h1>
-        <img src={imageURL} alt={"placeholder text"} />
-      </>
-    )
+    <>
+      <h1>An image</h1>
+      <img src={imageURL} alt={"placeholder text"} />
+    </>
   );
 };
 
