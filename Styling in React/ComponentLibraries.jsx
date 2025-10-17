b9793a2daf281dbed6cb21140de38ae77c122b33
hencci <hencci20@gmail.com>
2025-10-17 16:32:09 +0100

    Styling with component libraries

---
+++ Styling in React/ComponentLibraries.jsx
@@ -0,0 +1,5 @@
+// What if everything’s already done for you? Styling, behavior, and accessibility are taken care of for you in component libraries. As the name suggests, these libraries provide adaptable and reusable components that you can use directly in your project. These components include, but are not limited to, dropdowns, drawers, calendars, toggles, tabs, and all other components you can think of.
+
+// Material UI, Radix, and Chakra UI are worth a mention when talking about component libraries.
+
+// There are also icon component libraries like lucide react, which let you include icons in your project as components.
