2a13ec73d4cb4bf3f94b258e8f97693e489a06fc
hencci <hencci20@gmail.com>
2025-10-17 16:20:34 +0100

    Styling with CSS in JS

---
+++ Styling in React/CSS-in-JS.jsx
@@ -0,0 +1,5 @@
+// CSS-in-JS is a paradigm for styling front-end projects. It allows you to entirely take control of CSS with JavaScript and extend it with various features.
+
+// Additionally, it also helps to apply styling in a logical fashion, e.g. based on state, and also supports modular CSS in the same way that CSS Modules do.
+
+// There are various CSS-in-JS solutions. One of the most popular ones in the React ecosystem is styled-components.
