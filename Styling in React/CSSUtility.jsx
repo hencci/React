a4df8f7103aaef4bb4096d856ad71d66c399353f
hencci <hencci20@gmail.com>
2025-10-17 16:23:26 +0100

    Styling with CSS utility frameworks

---
+++ Styling in React/CSSUtility.jsx
@@ -0,0 +1,3 @@
+// CSS Utility Frameworks are a popular choice for styling React applications.
+
+// They provide a set of pre-defined classes that you can directly use in your HTML, or JSX in our case. Tailwind CSS is by far the most popular choice.
