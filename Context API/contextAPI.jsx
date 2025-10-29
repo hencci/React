57528af98475216a4f825ae4b46fa64a112f3c0a
hencci <hencci20@gmail.com>
2025-10-29 07:42:54 +0100

    Why we need Context API

---
+++ Context API/contextAPI.jsx
@@ -0,0 +1,7 @@
+{
+  /*   Why do we need Context API  */
+}
+
+// The Context API in React is a feature that allows you to manage the global state of your application without the need to pass data through multiple levels of components using props.
+
+// It provides a way to share data and functionality across different components, regardless of where they are located in the component tree.
