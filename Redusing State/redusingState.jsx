1c7fc7788804e3271a3a5ab06947850c033a5985
hencci <hencci20@gmail.com>
2025-11-01 07:49:36 +0100

    The useReducer hook

--- Redusing State/redusingState.jsx
+++ Redusing State/redusingState.jsx
@@ -35,3 +35,19 @@ function reducer(state, action) {
 // By using reducers, we can separate the state logic and even store it in a different file or directory, leading to smaller components that are easier to read.
 
 // Since reducers use actions, we can easily track any state-related bugs back to the dispatched action, and because our reducers are just pure functions, we can test them in isolation.
+
+{
+  /*    The useReducer hook    */
+}
+
+// React allows us to use reducers in our components through a hook called useReducer. This hook takes a reducer function and an initial state as arguments, then returns an array with two values: the current state and a dispatch function.
+
+// This dispatch function receives an action object as argument, which is passed to our reducer function and the returned value from it is used to update the state.
+
+const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+function handleClick() {
+  dispatch({ type: "incremented_count" });
+}
+
+// Similarly to how React updates state with the set function in useState, React only updates the state in the next render after calling the dispatch function.
